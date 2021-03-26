e36ed5223835044c75da7a5da289f3c728d64558
Vincent van Leeuwen <shoockz@hotmail.com>
2021-03-26 15:12:05 +0100

    styling fix

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ if(process.env.NODE_ENV !== 'production'){
 
 const port = process.env.PORT || 3000;
 const app = express();
-const serviceAccount = require("./serviceAccountKey.json");
+
 const firebaseConfig = {
   apiKey: process.env.FIREBASE_API_KEY,
   authDomain: process.env.FIREBASE_AUTH_DOMAIN,
@@ -27,7 +27,18 @@ const firebaseConfig = {
 
 admin.initializeApp({
   databaseURL: process.env.FIREBASE_DATABASE_URL,
-  credential: admin.credential.cert(serviceAccount)
+  credential: admin.credential.cert({
+    "type": "service_account",
+    "project_id": process.env.FIREBASE_PROJECT_ID,
+    "private_key_id": process.env.FIREBASE_PRIVATE_KEY_ID,
+    "private_key": process.env.FIREBASE_PRIVATE_KEY,
+    "client_email": process.env.FIREBASE_CLIENT_EMAIL,
+    "client_id": process.env.FIREBASE_CLIENT_ID,
+    "auth_uri": process.env.FIREBASE_AUTH_URI,
+    "token_uri": process.env.FIREBASE_TOKEN_URI,
+    "auth_provider_x509_cert_url": process.env.FIREBASE_AUTH_PROVIDER,
+    "client_x509_cert_url": process.env.FIREBASE_CLIENT_CERT
+  })
 });
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
